c0e98c928e2ee048b9ad5c0274d8da62664bb06a
Jerry Ng <ngshiheng@gmail.com>
2021-04-03 16:09:05 +0800

    Sanitize craftbeersg beer name and get product quantity

--- burplist/spiders/craftbeersg.py
+++ burplist/spiders/craftbeersg.py
@@ -1,3 +1,6 @@
+import re
+from typing import Tuple
+
 import scrapy
 from burplist.items import ProductItem
 from burplist.utils.proxy import get_proxy_url
@@ -7,21 +10,40 @@ from scrapy.loader import ItemLoader
 class CraftBeerSGSpider(scrapy.Spider):
     """
     Extract data from raw HTML
+    Product quantity might come in a Pack of 6, Pack of 16, Pack of 24 and etc.
     """
     name = 'craftbeersg'
-    start_urls = [get_proxy_url('https://craftbeersg.com/product-category/beer')]
+    start_urls = ['https://craftbeersg.com/product-category/beer']
+
+    def _get_product_name_quantity(self, raw_name: str) -> Tuple[str, int]:
+        name = raw_name.split('~', maxsplit=2)  # Example: "Magic Rock Brewing. Fantasma Gluten Free IPA ~ P198"
+        name = re.sub('[()]', '', name[0])  # Remove all parenthesis
+
+        if 'Pack of' in name:
+            name, quantity = name.split('Pack of', maxsplit=2)
+        elif 'Case of' in name:
+            name, quantity = name.split('Case of', maxsplit=2)
+        else:
+            quantity = 1
+
+        return name, int(quantity)
 
     def parse(self, response):
         products = response.xpath('//div[@class="product-inner"]')
 
         for product in products:
             loader = ItemLoader(item=ProductItem(), selector=product)
-            loader.add_xpath('name', './a[@class="product-loop-title"]/h3/text()')
+
+            raw_name = product.xpath('./a[@class="product-loop-title"]/h3/text()').get()
+            name, quantity = self._get_product_name_quantity(raw_name)
+
+            loader.add_value('name', name)
             loader.add_xpath('price', './span[@class="price"]/span/text()')
+            loader.add_value('quantity', quantity)
             loader.add_xpath('url', './a[@class="product-loop-title"]/@href')
             yield loader.load_item()
 
         # Recursively follow the link to the next page, extracting data from it
-        next_page = response.css('a.next.page-numbers').attrib['href']
+        next_page = response.css('a.next.page-numbers').attrib.get('href')
         if next_page is not None:
-            yield response.follow(get_proxy_url(next_page), callback=self.parse)
+            yield response.follow(next_page, callback=self.parse)
