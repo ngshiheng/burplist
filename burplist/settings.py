07db31a988313f5724d19d1b9ede6f80369ce0c7
Jerry Ng <ngshiheng@gmail.com>
2021-04-08 19:37:12 +0800

    Add colourized log

--- burplist/settings.py
+++ burplist/settings.py
@@ -1,14 +1,39 @@
+import copy
 import os
 
-# Scrapy settings for burplist project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+import scrapy.utils.log
+from colorlog import ColoredFormatter
 
+# Logging
+color_formatter = ColoredFormatter(
+    (
+        '%(log_color)s%(levelname)-5s%(reset)s '
+        '%(yellow)s[%(asctime)s]%(reset)s'
+        '%(white)s %(name)s %(funcName)s %(bold_purple)s:%(lineno)d%(reset)s '
+        '%(log_color)s%(message)s%(reset)s'
+    ),
+    datefmt='%y-%m-%d %H:%M:%S',
+    log_colors={
+        'DEBUG': 'blue',
+        'INFO': 'bold_cyan',
+        'WARNING': 'red',
+        'ERROR': 'bg_bold_red',
+        'CRITICAL': 'red,bg_white',
+    }
+)
+
+_get_handler = copy.copy(scrapy.utils.log._get_handler)
+
+
+def _get_handler_custom(*args, **kwargs):
+    handler = _get_handler(*args, **kwargs)
+    handler.setFormatter(color_formatter)
+    return handler
+
+
+scrapy.utils.log._get_handler = _get_handler_custom
+
+# Burplist
 BOT_NAME = 'burplist'
 
 SPIDER_MODULES = ['burplist.spiders']
