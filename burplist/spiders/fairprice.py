b1c44a9c28ffcce2b4a440a11d9df84ecf02790f
Jerry Ng <ngshiheng@gmail.com>
2021-04-09 22:12:59 +0800

    Fix bug where condition check should be 'is True' not 'is None'

--- burplist/spiders/fairprice.py
+++ burplist/spiders/fairprice.py
@@ -77,7 +77,7 @@ class FairPriceSpider(scrapy.Spider):
             yield loader.load_item()
 
         has_next_page = data['pagination']['page'] < data['pagination']['total_pages']
-        if has_next_page is not None:
+        if has_next_page is True:
             self.params['page'] += 1
             next_page = self.BASE_URL + urlencode(self.params)
             yield response.follow(next_page, callback=self.parse)
