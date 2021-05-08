9720886a47df4b9d41065a40d0e6bde1da6a8b7e
Jerry Ng <ngshiheng@gmail.com>
2021-05-08 09:40:27 +0800

    Refactoring

--- burplist/spiders/lazada.py
+++ burplist/spiders/lazada.py
@@ -1,60 +1,14 @@
 import logging
 import os
-import re
-from typing import Tuple
 from urllib.parse import urlencode
 
 import scrapy
 from burplist.items import ProductItem
+from burplist.utils.extractor import get_product_name_quantity
 from burplist.utils.proxy import get_proxy_url
 from scrapy.loader import ItemLoader
 
 logger = logging.getLogger(__name__)
-
-
-def get_product_name_quantity(raw_name: str) -> Tuple[str, int]:
-    logger.info(f'raw_name = "{raw_name}"')
-
-    # Hite Jinro Extra Cold Beer Single Carton
-    if 'carton' in raw_name.lower():
-        return raw_name, 24
-
-    # Carlsberg Danish Pilsner Beer Can 490ml (Pack of 24) Green Tab
-    is_pack = re.search(r'Pack of \d+', raw_name, flags=re.IGNORECASE)
-    if is_pack:
-        quantity = int(is_pack.group().split()[-1])
-        name = re.sub(is_pack.group(), '', raw_name)
-        return name, quantity
-
-    # Tiger Lager Beer Can 40x320ml, Guinness Foreign Extra Stout 24 x 500ml
-    is_ml = re.search(r'\d+\s?x\s?\d{3}ml', raw_name, flags=re.IGNORECASE)
-    if is_ml:
-        quantity = int(re.split('x', is_ml.group(), flags=re.IGNORECASE)[0])
-        name = re.sub(is_ml.group(), '', raw_name)
-        return name, quantity
-
-    # Heineken Beer 330ml x 24 can
-    is_ml_reverse = re.search(r'\d{3}ml\s?x\s?\d+', raw_name, flags=re.IGNORECASE)
-    if is_ml_reverse:
-        quantity = int(re.split('x', is_ml_reverse.group(), flags=re.IGNORECASE)[-1])
-        name = re.sub(is_ml_reverse.group(), '', raw_name)
-        return name, quantity
-
-    # Blue Moon Belgian White Wheat Ale 355ml x 24 Bottles
-    is_pack = re.search(r'\d+(?:[A-Za-z\s]|\s)+Bottles', raw_name, flags=re.IGNORECASE)
-    if is_pack:
-        quantity = int(is_pack.group().split()[0])
-        name = re.sub(is_pack.group(), '', raw_name)
-        return name, quantity
-
-    # San Miguel Pale Pilsen Can (24 cans x 330ml)
-    is_cans = re.search(r'\d+ Cans', raw_name, flags=re.IGNORECASE)
-    if is_cans:
-        quantity = int(is_cans.group().split()[0])
-        name = re.sub(is_cans.group(), '', raw_name)
-        return name, quantity
-
-    return raw_name, 1
 
 
 class LazadaSpider(scrapy.Spider):
--- burplist/spiders/redmart.py
+++ burplist/spiders/redmart.py
@@ -4,8 +4,7 @@ from urllib.parse import urlencode
 
 import scrapy
 from burplist.items import ProductItem
-from burplist.spiders.lazada import get_product_name_quantity
-from burplist.utils.proxy import get_proxy_url
+from burplist.utils.extractor import get_product_name_quantity
 from scrapy.loader import ItemLoader
 
 logger = logging.getLogger(__name__)
@@ -18,33 +17,32 @@ class RedMartSpider(scrapy.Spider):
     The API structure is similar to Lazada
     """
     name = 'redmart'
-    custom_settings = {'ROBOTSTXT_OBEY': False, 'DOWNLOAD_DELAY': os.environ.get('REDMART_DOWNLOAD_DELAY', 60)}
-    # BASE_URL = 'https://redmart.lazada.sg/shop-groceries-winesbeersspirits-beer-craftspecialtybeer/?'
-    BASE_URL = 'https://redmart.lazada.sg/shop-beer/?'
+    custom_settings = {'ROBOTSTXT_OBEY': True, 'DOWNLOAD_DELAY': os.environ.get('REDMART_DOWNLOAD_DELAY', 60)}
 
     params = {
         'ajax': 'true',
         'from': 'rm_nav_cate',
         'm': 'redmart',
-        'rating': 4,  # We filter products that have at least 4 stars and above
         'page': 1,
+        'rating': 4,  # We filter products that have at least 4 stars and above
     }
 
     headers = {
         'accept': 'application/json, text/plain, */*',
         'accept-encoding': 'gzip, deflate, br',
         'accept-language': '-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
-        'referer': f'{BASE_URL}page=' + str(params['page']),
+        # 'referer': 'https://redmart.lazada.sg/shop-beer/?from=rm_nav_cate&m=redmart&page=1&rating=3',
     }
 
     def start_requests(self):
-        url = self.BASE_URL + urlencode(self.params)
-        yield scrapy.Request(url=get_proxy_url(url), callback=self.parse, headers=self.headers)
+        for base_url in ['https://redmart.lazada.sg/shop-beer/', 'https://redmart.lazada.sg/shop-groceries-winesbeersspirits-beer-craftspecialtybeer/']:
+            url = f'{base_url}?{urlencode(self.params)}'
+            yield scrapy.Request(url=url, callback=self.parse, headers=self.headers)
 
     def parse(self, response):
         data = response.json()
         if 'rgv587_flag' in data:
-            raise ValueError(f'Rate limited by Lazada. URL <{response.request.url}>.')
+            raise ValueError(f'Rate limited by Red Mart. URL <{response.request.url}>.')
 
         products = data['mods']['listItems']
 
@@ -64,5 +62,5 @@ class RedMartSpider(scrapy.Spider):
 
             self.params['page'] += 1
             if int(data['mainInfo']['page']) < 5:  # We only scrape up to 5 pages for Red Mart. Anything beyond that are mostly trash
-                next_page = self.BASE_URL + urlencode(self.params)
-                yield response.follow(get_proxy_url(next_page), callback=self.parse, headers=self.headers)
+                next_page = f'{response.request.url}?{urlencode(self.params)}'
+                yield response.follow(next_page, callback=self.parse, headers=self.headers)
