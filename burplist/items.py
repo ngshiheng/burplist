62b3f97d539d4ace17f07f51dc590d1c40010d51
Jerry Ng <ngshiheng@gmail.com>
2021-05-15 11:46:23 +0800

    Add shopee spider

--- burplist/items.py
+++ burplist/items.py
@@ -6,17 +6,19 @@ from price_parser.parser import parse_price
 
 
 def parse_name(name: str) -> str:
-    """
-    Remove units from the product name
-    """
+
     name = re.sub(r'[,]$', '', name.strip())  # Remove trailing comma
-    remove_dashes = re.sub(r'-[\s?]', '', name)
-    remove_brackets = re.sub(r'[\(\[].*?[\]\)]', '', remove_dashes)  # E.g.: "Somersby Blackberry Cider [CANS] 330ml"
+    remove_brackets = re.sub(r'[\(\[].*?[\]\)]', '', name)  # E.g.: "Somersby Blackberry Cider [CANS] 330ml"
     remove_units = re.sub(r' \d*?s?\s?x?\s?\S+(ml)', '', remove_brackets, flags=re.IGNORECASE)  # E.g.: "Red Racer North West Pale Ale 320ml"
-    remove_abv = re.sub(r'ABV\s\d+.?\d*%', '', remove_units, flags=re.IGNORECASE)  # E.g.: Stone Sublimely Self Righteous Black IPA ABV 8.7%
+    remove_abv = re.sub(r'ABV\s?\d+.?\d*%', '', remove_units, flags=re.IGNORECASE)  # E.g.: Stone Sublimely Self Righteous Black IPA ABV 8.7%
     remove_trailing_s = re.sub(r'\d+s$', '', remove_abv, flags=re.IGNORECASE)  # FIXME: Figure out a better regex
 
-    return remove_trailing_s.strip()
+    remove_non_word_char = re.sub(r'[^a-zA-Z0-9%.]', ' ', remove_trailing_s)
+
+    # remove_trailing_comma = re.sub(r',$', '', remove_trailing_s)  # Remove trailing comma
+    remove_spaces = re.sub(' +', ' ', remove_non_word_char)
+
+    return remove_spaces.strip()
 
 
 class ProductItem(scrapy.Item):
