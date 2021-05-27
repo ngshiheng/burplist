ecaf325a16a28febb42102b7f3e940bc3b589fd1
Jerry Ng <ngshiheng@gmail.com>
2021-05-27 23:06:51 +0800

    Use follow_all

--- burplist/spiders/beerforce.py
+++ burplist/spiders/beerforce.py
@@ -17,11 +17,8 @@ class BeerForceSpider(scrapy.Spider):
     start_urls = ['https://beerforce.sg/pages/all-styles']
 
     def parse(self, response):
-        collections = response.xpath('//div[@class="o-layout__item u-1/2@tab u-1/4@desk"]')
-
-        for collection in collections:
-            url = response.urljoin(collection.xpath('./a/@href').get())
-            yield scrapy.Request(url=url, callback=self.parse_collection)
+        collections = response.xpath('//div[@class="o-layout__item u-1/2@tab u-1/4@desk"]//@href')
+        yield from response.follow_all(collections, callback=self.parse_collection)
 
     def parse_collection(self, response):
         product_details = response.xpath('//div[@class="product__details"]')
