50117ea0ba2de46938787d039a917603bc559473
Jerry Ng <ngshiheng@gmail.com>
2021-05-28 09:30:49 +0800

    Make thirsty spider start crawling from 'browse by beer style page' in order to get beer style

--- burplist/spiders/thirsty.py
+++ burplist/spiders/thirsty.py
@@ -1,6 +1,5 @@
 import logging
 import re
-from urllib.parse import urlencode
 
 import scrapy
 import sentry_sdk
@@ -14,33 +13,26 @@ class ThirstySpider(scrapy.Spider):
     """
     Extract data from raw HTML
     Do note that this site uses infinite scrolling
-
-    Additional product information:
-    - Stock Availability (inside)
-    - Style (name)
-    - Volume
-    - ABV
     """
     name = 'thirsty'
     custom_settings = {'ROBOTSTXT_OBEY': False}
-
-    BASE_URL = 'https://www.thirsty.com.sg/collections/beer?'
-
-    params = {
-        'view': 'json',
-        'sort_by': 'manual',
-        'page': 1,  # Starting page
-    }
+    start_urls = ['https://www.thirsty.com.sg/pages/shop-by-style']
 
     def _get_product_quantity(self, display_unit: str) -> int:
         quantity = re.split('x', display_unit, flags=re.IGNORECASE)  # E.g.: "DisplayUnit": "24 x 330ml". Note that 'x' can be capital letter
         return int(quantity[0]) if len(quantity) != 1 else 1
 
-    def start_requests(self):
-        url = self.BASE_URL + urlencode(self.params)
-        yield scrapy.Request(url=url, callback=self.parse)
-
     def parse(self, response):
+        collections = response.xpath('///a[@class="link-3 color-header"]')
+
+        for collection in collections:
+            style = collection.xpath('text()').get()
+            yield response.follow(collection, callback=self.parse_collection, meta={'style': style})
+
+    def parse_collection(self, response):
+        page = response.meta.get('page', 1)
+        current_url = response.request.url if page == 1 else response.meta['current_url']  # So that query parameters wont be appended whenever this method runs recursively
+
         products = response.xpath('//div[@class="text-left pr-0 col-8 col-tablet-12 tablet-pl-0 tablet-pr-0 col-desktop-12 desktop-pl-0 desktop-pr-0 cf"]')
 
         # NOTE: Because we don't have a way to determine if this request has next page, we would just stop following when `products` is not found
@@ -69,18 +61,18 @@ class ThirstySpider(scrapy.Spider):
                     loader.add_xpath('name', './/a[@class="link-3 color-header"]/text()')
                     loader.add_value('url', url)
 
-                    loader.add_xpath('brand', '//div[@class="text-left pr-0 col-8 col-tablet-12 tablet-pl-0 tablet-pr-0 col-desktop-12 desktop-pl-0 desktop-pr-0 cf"]/a/text()')
+                    loader.add_xpath('brand', './/a[@class="body-xs color-text"]/text()')
                     loader.add_xpath('origin', None)
-                    loader.add_xpath('style', '')
+                    loader.add_value('style', response.meta['style'])
 
-                    loader.add_xpath('abv', '//span[@class="alcohol color-text body-xs mr-5"]/text()')
-                    loader.add_xpath('volume', '//span[@class="volume color-text body-xs tablet-mr-5"]/text()')
+                    loader.add_xpath('abv', './/span[@class="alcohol color-text body-xs mr-5"]/text()')
+                    loader.add_xpath('volume', './/span[@class="volume color-text body-xs tablet-mr-5"]/text()')
                     loader.add_value('quantity', self._get_product_quantity(display_unit))
 
                     loader.add_value('price', price)
 
                     yield loader.load_item()
 
-            self.params['page'] += 1
-            next_page = self.BASE_URL + urlencode(self.params)
-            yield response.follow(next_page, callback=self.parse)
+            page += 1
+            next_page = f'{current_url}?view=json&sort_by=manual&page={page}'
+            yield response.follow(next_page, callback=self.parse_collection, meta={'current_url': current_url, 'page': page, 'style': response.meta['style']})
