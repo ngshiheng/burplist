600340d56415b4a101350b1425c32de688b0a9c6
Jerry Ng <ngshiheng@gmail.com>
2021-08-03 14:48:15 +0800

    Create zero state migration for existing database

---
+++ alembic/README.md
@@ -0,0 +1,9 @@
+# Alembic
+
+We use [alembic](https://alembic.sqlalchemy.org/en/latest/) to manage our database migrations for Burplist.
+
+Alembic is a lightweight database migration tool for usage with the SQLAlchemy Database Toolkit for Python.
+
+## How to generate migration for an existing database
+
+-   https://stackoverflow.com/a/56651578/10067850
--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,6 @@
 from logging.config import fileConfig
 
+from burplist.database.models import Base
 from sqlalchemy import engine_from_config, pool
 
 from alembic import context
@@ -16,7 +17,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
