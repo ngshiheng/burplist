d644dab7f79d601489748b9419ddca0d0e93c588
Jerry Ng <ngshiheng@gmail.com>
2021-09-19 09:55:55 +0800

    Add proxy unit test

---
+++ burplist/tests/test_proxy.py
@@ -0,0 +1,19 @@
+import unittest
+
+from burplist.utils.proxy import get_proxy_url
+from scrapy.utils.project import get_project_settings
+
+settings = get_project_settings()
+
+
+class TestProxy(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_get_proxy_url_returns_original_url_when_scraper_api_key_is_not_set(self) -> None:
+        assert settings.get('SCRAPER_API_KEY') is None
+
+        url = 'https://burplist.me'
+        proxied_url = get_proxy_url(url)
+
+        self.assertEqual(proxied_url, url)
