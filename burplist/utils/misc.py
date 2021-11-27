b70b0e57f0176b3bd5519b1f00bf6b1b6def71c3
Jerry Ng <ngshiheng@gmail.com>
2021-11-27 15:26:11 +0800

    refactor: Make stale_days >= 7 instead of > 0 to prevent accidental prune

--- burplist/utils/misc.py
+++ burplist/utils/misc.py
@@ -13,7 +13,7 @@ def remove_stale_products_prices(stale_days: int = 7) -> None:
     """
     Remove stale products and prices which are not updated for N number of days
     """
-    assert isinstance(stale_days, int) and stale_days > 0, 'Invalid `stale_days` input.'
+    assert isinstance(stale_days, int) and stale_days >= 7, 'Invalid `stale_days` input.'
 
     with Session() as session:
         stale_products = session.query(Product).filter(Product.updated_on <= datetime.utcnow() - timedelta(days=stale_days))
@@ -22,7 +22,7 @@ def remove_stale_products_prices(stale_days: int = 7) -> None:
 
         if stale_products_count < 1:
             logger.info('No stale products to delete.')
-            return None
+            return
 
         product_ids = [product.id for product in stale_products.all()]
         stale_prices = session.query(Price).filter(Price.product_id.in_(product_ids))
