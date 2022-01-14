327f6d29e3c610548b9e332794b47c5af05ef582
Jerry Ng <ngshiheng@gmail.com>
2022-01-15 02:34:12 +0800

    refactor: use XPATH with page object model (POM)

--- burplist/spiders/beerforce.py
+++ burplist/spiders/beerforce.py
@@ -2,6 +2,7 @@ import logging
 
 import scrapy
 from burplist.items import ProductLoader
+from burplist.locators.beerforce import BeerForceLocator
 
 logger = logging.getLogger(__name__)
 
@@ -24,7 +25,7 @@ class BeerForceSpider(scrapy.Spider):
         @url https://beerforce.sg/pages/all-styles
         @returns requests 1
         """
-        collections = response.xpath('//a[@class="collection-list__link"]/@href')
+        collections = response.xpath(BeerForceLocator.beer_collection)
         yield from response.follow_all(collections, callback=self.parse_collection)
 
     def parse_collection(self, response):
@@ -32,11 +33,11 @@ class BeerForceSpider(scrapy.Spider):
         @url https://beerforce.sg/collections/ipa
         @returns requests 1
         """
-        product_details = response.xpath('//div[@class="product-card__details"]')
-        product_media = response.xpath('//div[@class="product-card-top"]')
+        product_details = response.xpath(BeerForceLocator.product_details)
+        product_media = response.xpath(BeerForceLocator.product_media)
 
         for product, media in zip(product_details, product_media):
-            raw_price = product.xpath('.//span[@class="money"]/text()').get()
+            raw_price = product.xpath(BeerForceLocator.raw_price).get()
 
             if raw_price is None:
                 logger.info('Skipping item because it is sold out.')
@@ -45,32 +46,32 @@ class BeerForceSpider(scrapy.Spider):
             loader = ProductLoader(selector=product)
             loader.add_value('platform', self.name)
 
-            loader.add_xpath('name', './/h2[@class="product-card__title h4"]/text()')
-            loader.add_value('url', response.urljoin(media.xpath('./a/@href').get()))
+            loader.add_xpath('name', BeerForceLocator.product_name)
+            loader.add_value('url', response.urljoin(media.xpath(BeerForceLocator.product_url).get()))
 
-            loader.add_xpath('brand', './/h3[@class="product-card__vendor h6"]/text()')
+            loader.add_xpath('brand', BeerForceLocator.product_brand)
 
             loader.add_value('quantity', 1)
 
-            loader.add_xpath('price', './/span[@class="money"]/text()')
+            loader.add_xpath('price', BeerForceLocator.product_price)
 
             yield scrapy.Request(
-                response.urljoin(media.xpath('./a/@href').get()),
+                response.urljoin(media.xpath(BeerForceLocator.product_url).get()),
                 callback=self.parse_product_detail,
                 meta={'item': loader.load_item()},
                 dont_filter=False,
             )
 
         # Recursively follow the link to the next page, extracting data from it
-        has_next_page = response.xpath('//span[@class="next"]/a/@href').get()
+        has_next_page = response.xpath(BeerForceLocator.has_next_page).get()
         if has_next_page is not None:
-            next_page = response.urljoin(response.xpath('//span[@class="next"]/a/@href').get())
+            next_page = response.urljoin(response.xpath(BeerForceLocator.next_page).get())
             yield response.follow(next_page, callback=self.parse_collection)
 
     def parse_product_detail(self, response):
         loadernext = ProductLoader(item=response.meta['item'], response=response)
 
-        product_info = ''.join(response.xpath('//div[@class="product-single__content-text rte"]//*[b or strong]//text()').getall())
+        product_info = ''.join(response.xpath(BeerForceLocator.product_info).getall())
         style, volume, abv = product_info.split('|', maxsplit=2)
         loadernext.add_value('origin', None)
         loadernext.add_value('style', style)
@@ -78,7 +79,7 @@ class BeerForceSpider(scrapy.Spider):
         loadernext.add_value('abv', abv)
         loadernext.add_value('volume', volume)
 
-        image_url = response.xpath('//img[@class="product-single__photo__img js-pswp-img"]/@src').get()
+        image_url = response.xpath(BeerForceLocator.product_image_url).get()
         loadernext.add_value('image_url', f'https:{image_url}')
 
         yield loadernext.load_item()
