fc1ecfe99d5d160878edec7ad28c8c380379dec5
Jerry Ng <ngshiheng@gmail.com>
2022-06-26 10:28:07 +0800

    style: update docstring

--- burplist/utils/parsers.py
+++ burplist/utils/parsers.py
@@ -10,9 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 def parse_name(raw_name: str) -> str:
-    """
-    Sanitize product name
-    """
+    """Sanitize product name"""
     logger.info(f'Parsing raw_name "{raw_name}".')
 
     remove_brackets = re.sub(r'[\(\[].*?[\]\)]', '', raw_name)  # e.g. "Somersby Blackberry Cider [CANS] 330ml"
@@ -23,17 +21,17 @@ def parse_name(raw_name: str) -> str:
 
 
 def parse_style(raw_style: str) -> Optional[str]:
+    """Parse product style from existing product name"""
     return next((style for style in get_popular_styles() if style.lower() in raw_style.lower()), None)
 
 
 def parse_brand(raw_brand: str) -> Optional[str]:
+    """Parse product brand frome"""
     return next((brand for brand in get_popular_brands() if brand.lower() in raw_brand.lower()), None)
 
 
 def parse_abv(raw_abv: str) -> Optional[float]:
-    """
-    Get product abv from name
-    """
+    """Get product abv from name"""
     has_float_abv = re.search(r'(\d{1,2}\.\d{1,2})%', raw_abv)
     if has_float_abv:
         return float(has_float_abv.group(1))
@@ -46,9 +44,7 @@ def parse_abv(raw_abv: str) -> Optional[float]:
 
 
 def parse_volume(raw_name: str) -> Optional[int]:
-    """
-    Get product volume from name
-    """
+    """Parse product volume from name"""
     has_volume = re.search(r'(\d{3}) ?ml', raw_name, flags=re.IGNORECASE)
     if has_volume:
         return int(has_volume.group(1))
@@ -57,9 +53,7 @@ def parse_volume(raw_name: str) -> Optional[int]:
 
 
 def parse_quantity(raw_name: Union[str, int]) -> int:
-    """
-    Get product quantity from name
-    """
+    """Parse product quantity from name"""
     if isinstance(raw_name, int):
         return raw_name
 
@@ -87,9 +81,7 @@ def parse_quantity(raw_name: Union[str, int]) -> int:
 
 
 def quantize_price(price: Price) -> Price:
-    """
-    Quantize ProductItem price (type `Decimal`) so that it can be used for comparison in the pipeline
-    """
+    """Quantize ProductItem price (type `Decimal`) so that it can be used for comparison in the pipeline"""
     if price.amount:
         price.amount = price.amount.quantize(Decimal("1.00"))
 
