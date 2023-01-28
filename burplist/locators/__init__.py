2b687b784de6d16248a9c57435c6672da184e01c
Jerry Ng <ngshiheng@gmail.com>
2023-01-28 02:00:32 +0000

    BREAKING CHANGE: removed giant and shopee spiders

--- burplist/locators/__init__.py
+++ burplist/locators/__init__.py
@@ -1,17 +1,15 @@
 from .beerforce import BeerForceLocator
 from .coldstorage import ColdStorageLocator
-from .giant import GiantLocator
 from .greatbeerexperiment import TheGreatBeerExperimentLocator
 from .hopshop import HopShopLocator
 from .thirsty import ThirstyLocator
 from .troublebrewing import TroubleBrewingLocator
 
 __all__ = [
-    'BeerForceLocator',
-    'ColdStorageLocator',
-    'GiantLocator',
-    'TheGreatBeerExperimentLocator',
-    'HopShopLocator',
-    'ThirstyLocator',
-    'TroubleBrewingLocator',
+    "BeerForceLocator",
+    "ColdStorageLocator",
+    "TheGreatBeerExperimentLocator",
+    "HopShopLocator",
+    "ThirstyLocator",
+    "TroubleBrewingLocator",
 ]
