42f1c30e7ff834d33e93dc07e5612d03e2340c8a
Jerry Ng <ngshiheng@gmail.com>
2024-07-10 21:48:53 +0800

    fix(spider): update hopshop spider due to change in site html structure

--- burplist/locators/hopshop.py
+++ burplist/locators/hopshop.py
@@ -2,13 +2,16 @@ class HopShopLocator:
     """Locators for www.hopshop.com.sg"""
 
     # Locators for https://www.hopshop.com.sg/beer/
+    beer_collection = "//ul[@id='categories-navList']//li[@class='navList-item']/a"
     products = '//li[@class="product"]'
 
-    product_name = './/article/@data-name'
-    product_url = './/a/@href'
-    product_brand = './/article/@data-product-brand'
-    product_style = './/article/@data-product-category'
-    product_image_url = '//div[@class="card-img-container"]//img/@data-src'
-    product_price = './/article/@data-product-price'
+    # Locators for products page
+    # e.g. https://www.hopshop.com.sg/ipa/
+    product_name = ".//h3[@class='card-title']/a/text()"
+    product_url = ".//h3[@class='card-title']/a/@href"
+    product_brand = ".//div[@class='card-text card-text--brand']/text()"
+
+    product_image_url = './/div[@class="card-img-container"]/img/@src'
+    product_price = ".//span[contains(@class, 'price price--withoutTax price--main')]/text()"
 
     next_page = '//li[@class="pagination-item pagination-item--next"]/a/@href'
--- burplist/spiders/hopshop.py
+++ burplist/spiders/hopshop.py
@@ -20,38 +20,52 @@ class HopShopSpider(scrapy.Spider):
     start_urls = ["https://www.hopshop.com.sg/beer/"]
 
     def parse(self, response) -> Generator[scrapy.Request, None, None]:
+        collections = response.xpath(HopShopLocator.beer_collection)
+        for collection in collections:
+            style = collection.xpath("./text()").get().strip()
+            yield response.follow(
+                collection, callback=self.parse_collection, meta={"style": style}
+            )
+
+    def parse_collection(self, response) -> Generator[scrapy.Request, None, None]:
         """
-        @url https://www.hopshop.com.sg/beer/
-        @returns items 1
-        @returns requests 1 1
-        @scrapes platform name url brand volume quantity price
+        @url https://www.hopshop.com.sg/ipa/
         """
         products = response.xpath(HopShopLocator.products)
+        style = parse_style(response.meta["style"])
 
         for product in products:
             name = product.xpath(HopShopLocator.product_name).get()
-            style = parse_style(product.xpath(HopShopLocator.product_style).get()) or parse_style(name)
+            url = response.urljoin(product.xpath(HopShopLocator.product_url).get())
 
             loader = ProductLoader(selector=product)
 
             loader.add_value("platform", self.name)
             loader.add_value("name", name)
-            loader.add_value("url", response.urljoin(product.xpath(HopShopLocator.product_url).get()))
+            loader.add_value("url", url)
 
             loader.add_xpath("brand", HopShopLocator.product_brand)
             loader.add_xpath("origin", None)
             loader.add_value("style", style)
 
-            loader.add_value("abv", None)
+            loader.add_value("abv", name)
             loader.add_value("volume", name)
             loader.add_value("quantity", 1)
 
             loader.add_xpath("image_url", HopShopLocator.product_image_url)
 
-            loader.add_xpath("price", HopShopLocator.product_price)
+            price = self.get_product_price(
+                product.xpath(HopShopLocator.product_price).get()
+            )
+            loader.add_value("price", price)
+
             yield loader.load_item()
 
         has_next_page = response.xpath(HopShopLocator.next_page).get()
         if has_next_page:
             next_page = response.urljoin(has_next_page)
             yield response.follow(next_page, callback=self.parse)
+
+    @staticmethod
+    def get_product_price(raw_price: str) -> str:
+        return raw_price.split("-")[0]
